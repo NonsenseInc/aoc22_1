ed965eea9f6e69ae95dbf153613104ca5a8c7f21
Jan Haan <j.haan@medipee.com>
2022-12-01 21:33:43 +0100

    finished part 2

--- src/main.rs
+++ src/main.rs
@@ -21,12 +21,24 @@ fn main() -> io::Result<()> {
         /* add the calories of the line to the last elf */
         *(elfs.last_mut().unwrap()) += line.parse::<i32>().unwrap();
     }
-
     println!("{:?}", elfs);
 
-    /* figure out elf with max calories */
-    let max_index = elfs.iter().enumerate().max_by_key(|val| val.1).unwrap();
-    println!("{:?}", max_index);
+    /* figure out elf with max calories (part 1 answer) */
+    //let max_index = elfs.iter().enumerate().max_by_key(|val| val.1).unwrap();
+    //println!("{:?}", max_index);
+
+    /* figure out top three elfs by calories (part 2 answer) */
+    let mut top = [(0, 0), (0, 0), (0, 0)];
+    'elfs: for (index, elf) in elfs.iter().enumerate() {
+        for one in &mut top {
+            if elf > &one.1 {
+                one.0 = index;
+                one.1 = *elf;
+                continue 'elfs;
+            }
+        }
+    }
+    println!("{:?}", top);
 
     Ok(())
 }
